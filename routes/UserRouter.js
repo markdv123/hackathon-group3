143746887082d237ec04e608dd52c3120be208c6
ItinerantMonolith <ItinerantMonolith@gmail.com>
2020-12-03 17:33:23 -0500

    fix login route to be post

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -2,7 +2,7 @@ const Router = require('express').Router()
 const { createToken, verifyToken, getToken } = require('../middleware/jwtHandler')
 const controller = require('../controllers/UserController')
 
-Router.get('/login', controller.Login, createToken)
+Router.post('/login', controller.Login, createToken)
 Router.post('/create', controller.CreateUser, createToken)
 
 module.exports = Router
