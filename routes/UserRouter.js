8ddab7695ca56e32f05904122b4bde710ed4c9c1
ItinerantMonolith <ItinerantMonolith@gmail.com>
2020-12-03 14:28:42 -0500

    login and createUser paths built

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -1,7 +1,9 @@
 const Router = require('express').Router()
-
+const { createToken, verifyToken, getToken } = require('../middleware/jwtHandler')
 const controller = require('../controllers/UserController')
 
-Router.get('/:user_id', controller.GetUser)
+// Router.get('/view/:user_id', controller.GetUser)
+Router.get('/login', controller.Login, createToken)
+Router.post('/create', controller.CreateUser, createToken)
 
 module.exports = Router
