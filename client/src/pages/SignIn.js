a1f6968c132c531e28737e80c811d45dde23cebd
Mark DeVito <markdv@alumni.unc.edu>
2020-12-03 13:06:51 -0500

    signin and signup from class to functional components

--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -1,64 +1,58 @@
-import React, { Component } from 'react'
+import React from 'react'
 import TextInput from '../components/TextInput'
 import { __LoginUser } from '../services/UserServices'
-import {Link} from 'react-router-dom'
 
+function SignIn() {
+  const [email, updateEmail] = useState('')
+  const [password, updatePassword] = useState('')
+  const [formError, updateErr] = useState(false)
 
-export default class SignIn extends Component {
-    constructor() {
-      super()
-      this.state = {
-        email: '',
-        password: '',
-        formError: false
-      }
-    }
-  
-    handleChange = ({ target }) => {
-      this.setState({ [target.name]: target.value, formError: false })
-    }
-  
-    handleSubmit = async (e) => {
-      e.preventDefault()
-      try {
-        const loginData = await __LoginUser(this.state)
-        // console.log(loginData)
-        this.props.toggleAuthenticated(true, loginData.user, () =>
-          this.props.history.push('/')
-        )
-      } catch (error) {
-        this.setState({ formError: true })
-      }
-    }
-    render() {
-      const { email, password } = this.state
-      return (
-        <div className="signin flex-col">
-        
-          <form className="flex-col" onSubmit={this.handleSubmit}>
-            <TextInput
-              placeholder="Your Email"
-              name="email"
-              type="email"
-              value={email}
-              onChange={this.handleChange}
-            />
-            <TextInput
-              placeholder="Password"
-              name="password"
-              type="password"
-              value={password}
-              onChange={this.handleChange}
-            />
-            <button className="btn waves-effect waves-light red" type="submit" name="action">Sign In
-              <i className="material-icons right">fast_forward</i>
-            </button>
-            {this.state.formError ? <p>Error While Logging In</p> : <p></p>}
-          </form>
-          
-        </div>
-        
-      )
+  handleEmail = ({ target }) => {
+    updateEmail(target.value)
+    updateErr(false)
+  }
+
+  handlePassword = ({ target }) => {
+    updatePassword(target.value)
+    updateErr(false)
+  }
+
+  handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const loginData = await __LoginUser(email, password)
+      toggleAuthenticated(true, loginData.user)
+    } catch (error) {
+      this.setState({ formError: true })
     }
   }
-  
+  return (
+    <div className="signin flex-col">
+
+      <form className="flex-col" onSubmit={handleSubmit}>
+        <TextInput
+          placeholder="Your Email"
+          name="email"
+          type="email"
+          value={email}
+          onChange={handleEmail}
+        />
+        <TextInput
+          placeholder="Password"
+          name="password"
+          type="password"
+          value={password}
+          onChange={handlePassword}
+        />
+        <button className="btn waves-effect waves-light red" type="submit" name="action">Sign In
+              <i className="material-icons right">fast_forward</i>
+        </button>
+        {formError ? <p>Error While Logging In</p> : <p></p>}
+      </form>
+
+    </div>
+
+  )
+}
+
+export default SignIn
--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,79 +1,81 @@
-import React, { Component } from 'react'
+import React from 'react'
 import TextInput from '../components/TextInput'
 import { __RegisterUser } from '../services/UserServices'
-import {Link} from 'react-router-dom'
 
-export default class Signup extends Component {
-  // TODO Integrate Auth
-  constructor() {
-    super()
-    this.state = {
-      name: '',
-      email: '',
-      password: '',
-      accountTier: ''
-    }
+function SignUp() {
+  const [name, updateName] = useState('')
+  const [email, updateEmail] = useState('')
+  const [password, updatePassword] = useState('')
+  const [accountTier, updateTier] = useState('')
+
+  handleName = ({ target }) => {
+    updateName(target.value)
   }
 
-  handleChange = ({ target }) => {
-    this.setState({ [target.name]: target.value })
-    console.log(this.state)
+  handleEmail = ({ target }) => {
+    updateEmail(target.value)
+  }
+
+  handlePassword = ({ target }) => {
+    updatePassword(target.value)
+  }
+
+  handleTier = ({ target }) => {
+    updateTier(target.value)
   }
 
   handleSubmit = async (e) => {
     e.preventDefault()
     try {
-      await __RegisterUser(this.state)
-      this.props.history.push('/')
+      await __RegisterUser(name, email, password, accountTier)
+      props.history.push('/')
     } catch (error) {
       console.log(error)
     }
   }
-  render() {
-    const { name, password, email, accountTier } = this.state
-    return (
-      <div className="signup flex-col">
-        
-        <form className="flex-col" onSubmit={this.handleSubmit}>
-          <TextInput
-            placeholder="Your Name"
-            type="text"
-            name="name"
-            value={name}
-            onChange={this.handleChange}
-            />
-          <TextInput
-            placeholder="Your Email"
-            name="email"
-            value={email}
-            type="email"
-            onChange={this.handleChange}
-            />
-          
-          <TextInput
-            placeholder="Password"
-            type="password"
-            name="password"
-            value={password}
-            onChange={this.handleChange}
-            />
-            <div>
-                <label>Select Account Tier</label>
-                <select class="browser-default"
-                    name='accountTier'
-                    value={accountTier}
-                    onChange={this.handleChange}>
-                    <option value="" disabled selected>Choose your option</option>
-                    <option value="1">Platinum</option>
-                    <option value="2">Gold</option>
-                    <option value="3">Bronze</option>
-                </select>   
-            </div>
-          <button className="btn waves-effect waves-light red" type="submit" name="action">Sign Up
+  return (
+    <div className="signup flex-col">
+
+      <form className="flex-col" onSubmit={handleSubmit}>
+        <TextInput
+          placeholder="Your Name"
+          type="text"
+          name="name"
+          value={name}
+          onChange={handleName}
+        />
+        <TextInput
+          placeholder="Your Email"
+          name="email"
+          value={email}
+          type="email"
+          onChange={handleEmail}
+        />
+
+        <TextInput
+          placeholder="Password"
+          type="password"
+          name="password"
+          value={password}
+          onChange={handlePassword}
+        />
+        <div>
+          <label>Select Account Tier</label>
+          <select class="browser-default"
+            name='accountTier'
+            value={accountTier}
+            onChange={handleTier}>
+            <option value="" disabled selected>Choose your option</option>
+            <option value="1">Platinum</option>
+            <option value="2">Gold</option>
+            <option value="3">Bronze</option>
+          </select>
+        </div>
+        <button className="btn waves-effect waves-light red" type="submit" name="action">Sign Up
             <i className="material-icons right">directions_car</i>
-          </button>
-        </form>
-      </div>
-    )
-  }
+        </button>
+      </form>
+    </div>
+  )
+
 }
