41c12462f26f269b028ef46406037d6c13958179
ItinerantMonolith <ItinerantMonolith@gmail.com>
2020-12-03 12:19:08 -0500

    update users.email to be unique.  set up password/bcrypt"
    "

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,4 +1,33 @@
 const { User, Account } = require ('../models')
+const bcrypt = require('bcrypt')
+
+require('dotenv').config()
+const saltRounds = parseInt(process.env.SALT_ROUNDS)
+
+const {
+   checkPassword,
+   generatePassword
+ } = require('../middleware/passwordHandler')
+
+ // body is { email: <email>, password: <password> }
+
+const Login = async (req, resp) => {
+   try {
+      const email = req.body.email
+      const password = req.body.password
+      const user = await User.findOne( { where: { email: email } } )
+      if ( user && (await checkPassword( password, user.password) ) ) 
+        resp.send ( user )
+      else
+         resp.status(401).send({ msg: 'Unauthorized' })
+    } catch (err) {
+      throw err
+    }
+}
+
+
+
+
 
 const GetUser = async ( req, resp ) => {
    try {
